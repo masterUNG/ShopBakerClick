cea321597dab91189e427fae15c0f3ea9f7222cc
masterUNG <phrombutr@gmail.com>
2019-09-13 12:25:42 +0700

    Update

--- lib/screens/category.dart
+++ lib/screens/category.dart
@@ -18,9 +18,20 @@ class _CategoryState extends State<Category> {
     readFireStore();
   }
 
+  
+
   Future<void> readFireStore() async{
 
-    
+    Firestore firestore = Firestore.instance;
+    await firestore.collection('Shop').snapshots().listen((response) {
+      List<DocumentSnapshot> snapshots = response.documents;
+      for (var snapshot in snapshots) {
+        String nameDocument = snapshot.documentID;
+        print('nameDocument = $nameDocument');
+        String title = snapshot.data['Title'];
+        print('title = $title');
+      }
+    });
 
   }
 
